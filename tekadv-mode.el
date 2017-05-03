2e7adbbc8e6aa28d781f67903ceee2521eca5c34
yannick.suc <yannick.suc@epitech.eu>
2017-05-03 09:38:32 +0200

    all

--- tekadv-mode.el
+++ tekadv-mode.el
@@ -5,4 +5,13 @@
 
 (define-derived-mode tekadv-mode fundamental-mode "tekadv"
   "major mode for editing tekadv language code."
+  (global-set-key (kbd "C-<down>") 'tekadv-mode-move-next)
+  (global-set-key (kbd "C-<up>") 'tekadv-mode-move-prev)
+
+  (defun tekadv-mode-move-next ()
+    (interactive)
+    (re-search-forward "^[_a-zA-Z0-9\.\/]*:"))
+  (defun tekadv-mode-move-prev ()
+    (interactive)
+    (search-backward-regexp "^[_a-zA-Z0-9\.\/]*:"))
   (setq font-lock-defaults '(tekadv-highlights)))
